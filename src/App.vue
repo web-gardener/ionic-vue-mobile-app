946ecf4d7c41eeb8f8fe439dd00d7a260eb3388b
web-gardener <zhulishen0331@gmail.com>
2022-06-20 12:36:31 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <ion-app>
+    <h4 class="not-mobile">
+      Disenyado para experiencia movil
+    </h4>
+    <ion-router-outlet />
+  </ion-app>
+</template>
+
+<script lang="ts">
+import { IonApp, IonRouterOutlet } from '@ionic/vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    IonApp,
+    IonRouterOutlet
+  }
+});
+</script>
---
+++ src/components/CardListing.vue
@@ -0,0 +1,113 @@
+<template>
+    <div>
+        <ion-grid v-for="(item, index) in data" :key="index">
+            <ion-row>
+                <div class="company">
+                    {{ item.company }}
+                    <br />
+                    <span class="model">
+                        {{ item.model }}
+                    </span>
+                </div>
+                <img :src="item.image" />
+            </ion-row>
+            <ion-row>
+                <ion-col>
+                    <div class="label">Rent</div>
+                    <div class="value">
+                        {{ item.rate }}
+                    </div>
+                </ion-col>
+                <ion-col></ion-col>
+            </ion-row>
+        </ion-grid>
+    </div>
+</template>
+
+<script>
+import { IonCol, IonGrid, IonRow } from "@ionic/vue";
+export default {
+    name: "rent-page",
+    components: { IonGrid, IonRow, IonCol },
+    props: {
+        data: {
+            type: Array,
+            required: true,
+            default: () => [],
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+.company {
+    color: #ffffff;
+    font-weight: 500;
+    margin: 0 0 -64px 0;
+    padding: 15px;
+    z-index: 9999999999999;
+}
+
+.model {
+    font-size: 13px;
+    float: left;
+}
+
+ion-row {
+    img {
+        border-radius: 20px;
+        margin: 0;
+        padding: 0;
+    }
+
+    &.first-row {
+        border-radius: 20px 20px 0 0;
+        background: #ffffff;
+        margin: 0;
+        padding: 0;
+    }
+
+    &.second-row {
+        padding: 10px;
+        background: #ffffff;
+        border-radius: 0 0 20px 20px;
+    }
+
+    ion-col {
+        .label {
+            margin-bottom: 10px;
+            font-weight: 500;
+            font-size: 13px;
+        }
+
+        .value {
+            font-weight: 500;
+            font-size: 15px;
+            color: #96a6d2;
+        }
+
+        .unit {
+            font-size: 11px;
+        }
+    }
+}
+
+.balance,
+.expDate,
+.card-number {
+    text-align: left;
+}
+
+.balance,
+.card-number {
+    font-size: 20px;
+}
+
+.card-number {
+    font-size: 7vw;
+}
+
+.card-icon {
+    text-align: right;
+}
+</style>
